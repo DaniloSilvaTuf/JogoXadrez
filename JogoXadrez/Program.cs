d5a79df52e541f3885eceeefb6d2041ef541138a
Danilo Silva <danilo.silva.rp@gmail.com>
2026-03-10 12:17:55 -0300

    Testes iniciais com movimentos possiveis das pecas

--- JogoXadrez/Program.cs
+++ JogoXadrez/Program.cs
@@ -13,14 +13,16 @@ try
         Console.WriteLine();
         Console.Write("Origem: ");
         Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+        bool[,] posicoesPossiveis = partida.tab.peca(origem).movimentosPossiveis();
+
+        Console.Clear();
+        Tela.imprimirTabuleiro(partida.tab, posicoesPossiveis);
 
         Console.Write("Destino: ");
         Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
 
         partida.executaMovimento(origem, destino);
     }
-    
-
 }
 catch (TabuleiroException e)
 {
