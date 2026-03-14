8b5671d140045f1466d08c2875fce56a08ca3b7b
Danilo Silva <danilo.silva.rp@gmail.com>
2026-03-14 12:56:48 -0300

    Jogada especial roque pequeno e rodque grande

--- JogoXadrez/xadrez/Peao.cs
+++ JogoXadrez/xadrez/Peao.cs
@@ -45,42 +45,42 @@ namespace xadrez
                 }
 
                 pos.definirValores(posicao.linha - 1, posicao.coluna - 1);
-                if (tab.posicaoValida(pos) && livre(pos) && existeInimigo(pos))
+                if (tab.posicaoValida(pos) && existeInimigo(pos))
                 {
                     mat[pos.linha, pos.coluna] = true;
                 }
 
                 pos.definirValores(posicao.linha - 1, posicao.coluna + 1);
-                if (tab.posicaoValida(pos) && livre(pos) && existeInimigo(pos))
+                if (tab.posicaoValida(pos) && existeInimigo(pos))
+                {
+                    mat[pos.linha, pos.coluna] = true;
+                }
+            }
+
+            else
+            {
+                pos.definirValores(posicao.linha + 1, posicao.coluna);
+                if (tab.posicaoValida(pos) && livre(pos))
                 {
                     mat[pos.linha, pos.coluna] = true;
                 }
 
-                else
+                pos.definirValores(posicao.linha + 2, posicao.coluna);
+                if (tab.posicaoValida(pos) && livre(pos) && qtdMovimentos == 0)
                 {
-                    pos.definirValores(posicao.linha + 1, posicao.coluna);
-                    if (tab.posicaoValida(pos) && livre(pos))
-                    {
-                        mat[pos.linha, pos.coluna] = true;
-                    }
+                    mat[pos.linha, pos.coluna] = true;
+                }
 
-                    pos.definirValores(posicao.linha + 2, posicao.coluna);
-                    if (tab.posicaoValida(pos) && livre(pos) && qtdMovimentos == 0)
-                    {
-                        mat[pos.linha, pos.coluna] = true;
-                    }
+                pos.definirValores(posicao.linha + 1, posicao.coluna - 1);
+                if (tab.posicaoValida(pos) && existeInimigo(pos))
+                {
+                    mat[pos.linha, pos.coluna] = true;
+                }
 
-                    pos.definirValores(posicao.linha + 1, posicao.coluna - 1);
-                    if (tab.posicaoValida(pos) && livre(pos) && existeInimigo(pos))
-                    {
-                        mat[pos.linha, pos.coluna] = true;
-                    }
-
-                    pos.definirValores(posicao.linha + 1, posicao.coluna + 1);
-                    if (tab.posicaoValida(pos) && livre(pos) && existeInimigo(pos))
-                    {
-                        mat[pos.linha, pos.coluna] = true;
-                    }
+                pos.definirValores(posicao.linha + 1, posicao.coluna + 1);
+                if (tab.posicaoValida(pos) && existeInimigo(pos))
+                {
+                    mat[pos.linha, pos.coluna] = true;
                 }
             }
             return mat;
